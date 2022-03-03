fd00485ba5900f428299e1fdf84ee5676089104d
RAVEENA K <77202982+RaveenaK-06@users.noreply.github.com>
2022-03-03 10:55:46 +0530

    Add Standard Design

--- StudentAttendanceManagementSystem/StudentAttendanceManagementSystem/Views/AddStandard.xaml.cs
+++ StudentAttendanceManagementSystem/StudentAttendanceManagementSystem/Views/AddStandard.xaml.cs
@@ -24,5 +24,15 @@ namespace StudentAttendanceManagementSystem.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
