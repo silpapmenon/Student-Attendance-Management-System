412d833b19005505ce491ac22880bdf136538021
Rahana121 <90402976+Rahana121@users.noreply.github.com>
2022-03-03 13:03:47 +0530

    AddDivision Updated

--- StudentAttendanceManagementSystem/StudentAttendanceManagementSystem/Views/AddDivision.xaml.cs
+++ StudentAttendanceManagementSystem/StudentAttendanceManagementSystem/Views/AddDivision.xaml.cs
@@ -24,5 +24,20 @@ namespace StudentAttendanceManagementSystem.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
