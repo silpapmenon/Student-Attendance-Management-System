d3def03cedc4ef38cc0ea148e101b7189dd5f34a
RAVEENA K <77202982+RaveenaK-06@users.noreply.github.com>
2022-03-04 12:08:10 +0530

    Added AdminModel Properties

---
+++ StudentAttendanceManagementSystem/EntityLayer/Model/AdminModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Model
+{
+    public class AdminModel
+    {
+        private string _username;
+        private string _password;
+        private int _userID;
+        public string Username { get => _username; set => _username = value; }
+        public string Password { get => _password; set => _password = value; }
+        public int UserID { get => _userID; set => _userID = value; }
+    }
+}
