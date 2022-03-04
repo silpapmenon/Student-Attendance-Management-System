02c8c18783ba609402cff6e06e3155aedd89f2ac
RAVEENA K <77202982+RaveenaK-06@users.noreply.github.com>
2022-03-04 12:28:57 +0530

    Added GenderModel Properties

---
+++ StudentAttendanceManagementSystem/EntityLayer/Model/GenderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Model
+{
+    public class GenderModel
+    {
+        private int _genderID;
+        private string _genderName;
+        public int GenderID { get => _genderID; set => _genderID = value; }
+        public string GenderName { get => _genderName; set => _genderName = value; }
+    }
+}
