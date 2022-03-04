2af58591d0a2f6f0e4a695e50c0623f9ec0a83dd
silpapmenon <97158489+silpapmenon@users.noreply.github.com>
2022-03-04 12:17:03 +0530

    Added AttendanceModel properties

---
+++ StudentAttendanceManagementSystem/EntityLayer/Model/AttendanceModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Model
+{
+    public class AttendanceModel
+    {
+        private int _attendanceId;
+        private int _studentId;
+        private string _divisionName;
+        private DateTime _attendanceDate;
+        private bool _status;
+        private int _staffId;
+
+
+        public int AttendanceID { get => _attendanceId; set => _attendanceId = value; }
+        public int StudentID { get => _studentId; set => _studentId = value; }
+        public string DivisionName { get => _divisionName; set => _divisionName = value; }
+        public DateTime AttendanceDate { get => _attendanceDate; set => _attendanceDate = value; }
+        public bool Status { get => _status; set => _status = value; }
+        public int StaffID { get => _staffId; set => _staffId = value; }
+    }
+}
+
