9850984d9cfba02ac633d799b8352adf1e58e0d0
Rahana121 <90402976+Rahana121@users.noreply.github.com>
2022-03-04 12:25:38 +0530

    Add Subectmodel properties

---
+++ StudentAttendanceManagementSystem/EntityLayer/Model/SubjectModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Model
+{
+    public class SubjectModel
+    {
+        private int _subjectID;
+        private string _subjectName;
+        public int SujectID { get => _subjectID; set => _subjectID = value; }
+        public string SubjectName { get => _subjectName; set => _subjectName = value; }
+
+    }
+}
