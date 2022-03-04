5424ee4a610650cfdf0b9667718e282cdf4b783e
Rahana121 <90402976+Rahana121@users.noreply.github.com>
2022-03-04 12:22:33 +0530

    Add Roles Properties

---
+++ StudentAttendanceManagementSystem/EntityLayer/Model/RolesModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Model
+{
+    public class RolesModel
+    {
+        private int _roleID;
+        private string _roleName;
+        public int RoleID
+        { get => _roleID; set => _roleID = value; }
+        public string RoleName
+        { get => _roleName; set => _roleName = value; }
+    }
+}
